395801a202f2f325c9def86f79dea10aad183d99
Ivan Kostrubin <7spawn@gmail.com>
2023-04-19 22:41:31 +0300

    init

---
+++ common-dotfiles.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt install \
+  git       \
+  neovim    \
+  ranger    \
+  tmux      \
+  zsh       \
+
+git clone https://github.com/ovitente/common-dotfiles.git
---
+++ readme.md
@@ -0,0 +1,1 @@
+This project follows goal to have one liner that will install user friendly cli tools to work on any server.
